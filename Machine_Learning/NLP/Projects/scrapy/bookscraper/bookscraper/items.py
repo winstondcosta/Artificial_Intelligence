f334e075af274e7bc4d498897c9ed4b34f1a693f
Winston Dcosta <46948960+winstondcosta@users.noreply.github.com>
2023-08-21 12:21:33 +0530

    Add files via upload

---
+++ Machine_Learning/NLP/Projects/scrapy/bookscraper/bookscraper/items.py
@@ -0,0 +1,18 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy import Field
+
+
+class BookscraperItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = Field()
+    price = Field()
+    upc = Field()
+    image_url = Field()
+    url = Field()
+    pass
